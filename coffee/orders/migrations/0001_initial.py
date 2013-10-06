21ec3019d161a1a719bd2169b8b4703d863f7357
chryso <chryso@gmail.com>
2013-10-06 14:16:49 -0400

    ui changes

--- coffee/orders/migrations/0001_initial.py
+++ coffee/orders/migrations/0001_initial.py
@@ -29,9 +29,9 @@ class Migration(SchemaMigration):
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=128)),
             ('description', self.gf('django.db.models.fields.CharField')(max_length=1024)),
-            ('one_pound_price', self.gf('django.db.models.fields.DecimalField')(decimal_places=2, max_digits=5)),
-            ('two_pound_price', self.gf('django.db.models.fields.DecimalField')(decimal_places=2, max_digits=5)),
-            ('five_pound_price', self.gf('django.db.models.fields.DecimalField')(decimal_places=2, max_digits=5)),
+            ('one_pound_price', self.gf('django.db.models.fields.DecimalField')(max_digits=5, decimal_places=2)),
+            ('two_pound_price', self.gf('django.db.models.fields.DecimalField')(max_digits=5, decimal_places=2)),
+            ('five_pound_price', self.gf('django.db.models.fields.DecimalField')(max_digits=5, decimal_places=2)),
             ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
         ))
         db.send_create_signal('orders', ['Coffee'])
@@ -41,7 +41,9 @@ class Migration(SchemaMigration):
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=128)),
             ('team', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['orders.Team'])),
-            ('date', self.gf('django.db.models.fields.DateField')(auto_now_add=True, unique=True, blank=True)),
+            ('date', self.gf('django.db.models.fields.DateField')(auto_now_add=True, blank=True, unique=True)),
+            ('placed', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('closed', self.gf('django.db.models.fields.BooleanField')(default=False)),
         ))
         db.send_create_signal('orders', ['Order'])
 
@@ -54,6 +56,7 @@ class Migration(SchemaMigration):
             ('size', self.gf('django.db.models.fields.IntegerField')(default=1)),
             ('quantity', self.gf('django.db.models.fields.IntegerField')()),
             ('personal', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('paid', self.gf('django.db.models.fields.DecimalField')(decimal_places=2, max_digits=5, default=0.0)),
         ))
         db.send_create_signal('orders', ['OrderItem'])
 
@@ -79,18 +82,20 @@ class Migration(SchemaMigration):
         'orders.coffee': {
             'Meta': {'object_name': 'Coffee'},
             'description': ('django.db.models.fields.CharField', [], {'max_length': '1024'}),
-            'five_pound_price': ('django.db.models.fields.DecimalField', [], {'decimal_places': '2', 'max_digits': '5'}),
+            'five_pound_price': ('django.db.models.fields.DecimalField', [], {'max_digits': '5', 'decimal_places': '2'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'one_pound_price': ('django.db.models.fields.DecimalField', [], {'decimal_places': '2', 'max_digits': '5'}),
-            'two_pound_price': ('django.db.models.fields.DecimalField', [], {'decimal_places': '2', 'max_digits': '5'})
+            'one_pound_price': ('django.db.models.fields.DecimalField', [], {'max_digits': '5', 'decimal_places': '2'}),
+            'two_pound_price': ('django.db.models.fields.DecimalField', [], {'max_digits': '5', 'decimal_places': '2'})
         },
         'orders.order': {
             'Meta': {'object_name': 'Order'},
-            'date': ('django.db.models.fields.DateField', [], {'auto_now_add': 'True', 'unique': 'True', 'blank': 'True'}),
+            'closed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'date': ('django.db.models.fields.DateField', [], {'auto_now_add': 'True', 'blank': 'True', 'unique': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'placed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'team': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['orders.Team']"})
         },
         'orders.orderitem': {
@@ -98,6 +103,7 @@ class Migration(SchemaMigration):
             'coffee': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['orders.Coffee']"}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'order': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['orders.Order']"}),
+            'paid': ('django.db.models.fields.DecimalField', [], {'decimal_places': '2', 'max_digits': '5', 'default': '0.0'}),
             'person': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['orders.Person']"}),
             'personal': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'quantity': ('django.db.models.fields.IntegerField', [], {}),
