69892a931845c8488c8682e62c800f8a1725780b
chryso <chryso@gmail.com>
2013-10-08 13:56:11 -0400

    added uwsgi ini file

--- coffee/coffee/settings.py
+++ coffee/coffee/settings.py
@@ -65,7 +65,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-#STATIC_ROOT = join(this_dir, 'static')
+STATIC_ROOT = join(this_dir, 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
