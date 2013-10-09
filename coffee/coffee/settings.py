c8b32c491234a4bbdfeba335ece4c4b9039679ba
chryso <chryso@gmail.com>
2013-10-08 23:33:12 -0400

    updated settings for static files

--- coffee/coffee/settings.py
+++ coffee/coffee/settings.py
@@ -4,7 +4,8 @@ from os.path import abspath, dirname, join
 
 this_dir = dirname(abspath(__file__))
 
-DEBUG = True
+DEBUG = False
+#DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -27,7 +28,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.chryso.net']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
